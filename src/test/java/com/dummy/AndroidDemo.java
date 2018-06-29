9b8050448ee4d4561209d12cf92392025b851bf6
Ketan Sethi <ketansethi786@gmail.com>
2018-06-29 17:51:37 +0530

    test

--- src/test/java/com/dummy/AndroidDemo.java
+++ src/test/java/com/dummy/AndroidDemo.java
@@ -15,10 +15,11 @@ import org.testng.annotations.Test;
 
 import io.appium.java_client.MobileElement;
 import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.android.AndroidElement;
 import io.appium.java_client.remote.AndroidMobileCapabilityType;
 import io.appium.java_client.remote.MobileCapabilityType;
 
-public class AppiumDemo {
+public class AndroidDemo {
 
 	public WebDriver driver;
 
@@ -27,11 +28,10 @@ public class AppiumDemo {
 		DesiredCapabilities capabilities = new DesiredCapabilities();
 		capabilities.setCapability(MobileCapabilityType.PLATFORM_NAME, Platform.ANDROID);
 		capabilities.setCapability(MobileCapabilityType.DEVICE_NAME, "Android Emulator");
-		capabilities.setCapability(MobileCapabilityType.APP, new File("./src/test/resources/WordPress.apk"));
+		capabilities.setCapability(MobileCapabilityType.APP, new File("./src/test/resources/ApiDemos-debug.apk"));
 
-		capabilities.setCapability(AndroidMobileCapabilityType.APP_PACKAGE, "org.wordpress.android");
-		capabilities.setCapability(AndroidMobileCapabilityType.APP_ACTIVITY,
-				"org.wordpress.android.ui.WPLaunchActivity");
+		capabilities.setCapability(AndroidMobileCapabilityType.APP_PACKAGE, "io.appium.android.apis");
+		capabilities.setCapability(AndroidMobileCapabilityType.APP_ACTIVITY, ".app.AlertDialogSamples");
 
 		driver = new AndroidDriver<MobileElement>(new URL("http://0.0.0.0:4723/wd/hub"), capabilities);
 		driver.manage().timeouts().implicitlyWait(80, TimeUnit.SECONDS);
@@ -41,9 +41,9 @@ public class AppiumDemo {
 	@Test
 	public void testApp() {
 
-		driver.findElement(By.xpath("//android.widget.Button[@text='LOG IN']")).click();
-		driver.findElement(By.xpath("//android.widget.EditText")).sendKeys("ketansethi786@gmail.com");
-		driver.findElement(By.id("org.wordpress.android:id/primary_button")).click();
+		AndroidElement openDialogButton = (AndroidElement) driver
+				.findElement(By.id("io.appium.android.apis:id/two_buttons"));
+		openDialogButton.click();
 
 	}
 
